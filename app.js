0eb8d167f5ad1a7e484c861a7b05e7d9d5ae4bde
Oscar Rey Tajes <oscar.rey.tajes@gmail.com>
2021-11-13 10:54:59 +0100

    Add Dark Mode switcher and styles

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
 import signature from './modules/signature.js'
+// signature()
 
-signature()
+let darkMode = JSON.parse(localStorage.getItem('darkmode')) || false
+
+const getdarkMode = () => {
+
+    if (localStorage.getItem('darkmode')){
+        darkMode = JSON.parse(localStorage.getItem('darkmode'))
+    }
+    else {
+        localStorage.setItem('darkmode', JSON.stringify(darkMode))
+    }
+    return darkMode
+}
+
+const toggleDarkMode = () => {
+    localStorage.setItem('darkmode',JSON.stringify(!getdarkMode()))
+    setDarkModeCssClass()
+    // getdarkMode() ? document.querySelector('body').classList.add('darkmode') : document.querySelector('body').classList.remove('darkmode')
+}
+
+const setDarkModeCssClass = () => {
+    getdarkMode() ? document.querySelector('body').classList.add('darkmode') : document.querySelector('body').classList.remove('darkmode')
+}
+
+const setDarkModeSwitcher = () => {
+    document.querySelectorAll('.darkModeToggle').forEach((el)=>{
+        el.addEventListener('click', (e)=>{
+            e.preventDefault()
+            toggleDarkMode()
+        })
+    })
+
+    console.log('DARKMODE', getdarkMode());
+}
+
+
+setDarkModeSwitcher()
+//auto set darkmode on load
+setDarkModeCssClass()
